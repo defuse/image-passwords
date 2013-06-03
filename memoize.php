a6075d0afa2501df016c006e4b9545f406f608f0
Taylor Hornby <havoc@defuse.ca>
2013-06-02 22:55:17 -0600

    Add more images and increase length to 12.

--- memoize.php
+++ memoize.php
@@ -43,7 +43,7 @@ function canvasSupport() {
 
 function canvasApp() {
 
-    var MAX_KEY_LENGTH = 10;
+    var MAX_KEY_LENGTH = 12;
     var theCanvas = document.getElementById("canvasOne");
     var context = theCanvas.getContext("2d");
 
